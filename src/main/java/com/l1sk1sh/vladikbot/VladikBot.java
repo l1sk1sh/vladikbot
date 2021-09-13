078645294eda1c5f524d998d3077a1c2cc285757
ok <>
2021-09-13 12:44:35 +0300

    Update dependencies

--- src/main/java/com/l1sk1sh/vladikbot/VladikBot.java
+++ src/main/java/com/l1sk1sh/vladikbot/VladikBot.java
@@ -86,7 +86,7 @@ class VladikBot {
 
             JDA jda = JDABuilder.create(settings.get().getToken(), Const.REQUIRED_INTENTS)
                     .enableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE)
-                    .disableCache(CacheFlag.ACTIVITY, CacheFlag.CLIENT_STATUS, CacheFlag.EMOTE)
+                    .disableCache(CacheFlag.ACTIVITY, CacheFlag.CLIENT_STATUS, CacheFlag.EMOTE, CacheFlag.ONLINE_STATUS)
                     .setBulkDeleteSplittingEnabled(true)
                     .build();
 
