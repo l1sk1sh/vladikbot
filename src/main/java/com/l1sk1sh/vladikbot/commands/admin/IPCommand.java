b064123c8a07df12b56c2f370ad5d481be31f4fc
ok <@>
2024-11-26 16:55:37 +0100

    Add external IP to ip command

--- src/main/java/com/l1sk1sh/vladikbot/commands/admin/IPCommand.java
+++ src/main/java/com/l1sk1sh/vladikbot/commands/admin/IPCommand.java
@@ -4,6 +4,12 @@ import com.jagrosh.jdautilities.command.SlashCommandEvent;
 import com.l1sk1sh.vladikbot.settings.BotSettingsManager;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * @author l1sk1sh
  */
@@ -23,11 +29,24 @@ public class IPCommand extends AdminCommand {
         String ip = settings.get().getBotPublicIP();
 
         if (ip == null || ip.isEmpty()) {
-            event.replyFormat("%1$s Public IP was not set. Ask owner of the bot for details", event.getClient().getWarning()).setEphemeral(true).queue();
-
-            return;
+            ip = "not set";
         }
 
-        event.replyFormat("Public IP is `%1$s`", ip).setEphemeral(true).queue();
+        String externalIp = null;
+
+        try {
+            URL checkIP = new URL("https://checkip.amazonaws.com");
+
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(
+                    checkIP.openStream()))) {
+                externalIp = in.readLine();
+            }
+        } catch (MalformedURLException ignored) {
+        } catch (IOException e) {
+            event.replyFormat("%1$s Error occurred: `%2$s`", event.getClient().getError(), e.getLocalizedMessage()).setEphemeral(true).queue();
+        }
+
+        String externalIpMessage = (externalIp != null) ? "\nExternal IP address (might be hidden by proxy or VPN) is: " + externalIp : "";
+        event.replyFormat("Public IP (set manually by owner) is: `%1$s`%2$s", ip, externalIpMessage).setEphemeral(true).queue();
     }
 }
